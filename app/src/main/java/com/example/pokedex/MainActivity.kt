b422bd02bbdfdfb585d93c2fe2f23d4261618afd
Mark Roth <mzroth@gmail.com>
2019-04-02 22:00:56 -0400

    I got the first GET in the API working and have a list of Pokemon returned and displayed

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -5,22 +5,43 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.widget.Toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.lang.IllegalStateException
 
 class MainActivity : AppCompatActivity(), PokemonListRecyclerViewAdapter.PokemonListRecyclerViewClickListener {
 
     lateinit var pokemonListRecyclerView: RecyclerView
+    lateinit var pokeService: PokeApi
+    private var list: List<NameUrl> = listOf()
 
     companion object {
         const val INTENT_POKEMON_KEY = "pokemonNumber"
+        const val POKEMON_TOTAL_NUMBER = "807"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        pokeService = PokeApi.create()
         pokemonListRecyclerView = findViewById(R.id.pokemon_list_recycler_view)
-        pokemonListRecyclerView.adapter = PokemonListRecyclerViewAdapter(this)
+        pokemonListRecyclerView.adapter = PokemonListRecyclerViewAdapter(list, this)
         pokemonListRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        pokeService.getAllPokemon(POKEMON_TOTAL_NUMBER).enqueue(object: Callback<AllPokemon> {
+            override fun onResponse(call: Call<AllPokemon>, response: Response<AllPokemon>) {
+                list = response.body()?.results ?: throw IllegalStateException()
+                val recyclerViewAdapter = pokemonListRecyclerView.adapter as? PokemonListRecyclerViewAdapter
+                recyclerViewAdapter?.updateList(list)
+            }
+
+            override fun onFailure(call: Call<AllPokemon>, t: Throwable) {
+                Toast.makeText(this@MainActivity, "Please close the app and try again", Toast.LENGTH_LONG).show()
+            }
+        })
     }
 
     override fun pokemonClicked(number: Int) {
---
+++ app/src/main/java/com/example/pokedex/PokeApi.kt
@@ -0,0 +1,23 @@
+package com.example.pokedex
+
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface PokeApi {
+    @GET("pokemon")
+    fun getAllPokemon(@Query("limit") limit: String): Call<AllPokemon>
+
+    companion object {
+        fun create(): PokeApi {
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("https://pokeapi.co/api/v2/")
+                .build()
+            return retrofit.create(PokeApi::class.java)
+        }
+    }
+}
